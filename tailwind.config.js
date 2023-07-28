36653f5141098632bf6e8da13f745244b52d72d0
Dolan666/ <dolanbiswas44@gmail.com>
2023-07-28 20:27:11 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  ul li a{
+    @apply rounded py-[7px] px-[13px] text-[17px]
+  }
